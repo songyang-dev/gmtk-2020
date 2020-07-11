029edd26a97c92edcc9480ab7a8d8cac2af2961a
Carl Summers <31711543+CSumm@users.noreply.github.com>
2020-07-11 19:20:13 -0400

    Add player controller and ai

---
+++ Assets/InputActions/PlayerController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator anim;
+
+    private PlayerInteraction inputActions;
+    
+    private Vector2 movementInput;
+
+    [SerializeField]
+    private float moveSpeed = 10f;
+    private Vector3 inputDirection;
+    private Vector3 moveVector;
+    Vector2 lookPositon;
+
+    private Quaternion currentRotation;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        inputActions = new PlayerInteraction();
+        inputActions.Player.Move.performed += context => movementInput = context.ReadValue<Vector2>();
+    }
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        float h = movementInput.x;
+        float v = movementInput.y;
+
+        Vector3 targetInput = new Vector3(h, 0, v);
+        inputDirection = Vector3.Lerp(inputDirection, targetInput, Time.deltaTime * 10f);
+
+        Vector3 camForward = Camera.main.transform.forward;
+        Vector3 camRight = Camera.main.transform.right;
+        camForward.y = 0f;
+        camRight.y = 0f;
+
+        Vector3 desiredDirection = camForward * inputDirection.z + camRight * inputDirection.x;
+
+        Move(desiredDirection);
+        Turn(desiredDirection);
+        AnimatePlayer(desiredDirection);
+    }
+
+    void Move(Vector3 desiredDirection)
+    {
+        moveVector.Set(desiredDirection.x, 0f, desiredDirection.z);
+        moveVector = moveVector * moveSpeed * Time.deltaTime;
+        transform.position += moveVector;
+    }
+
+    void Turn(Vector3 desiredDirection)
+    {
+        if((desiredDirection.x > 0.1 || desiredDirection.x < -0.1) || (desiredDirection.z > 0.1 || desiredDirection.z < -0.1)){
+            currentRotation = Quaternion.LookRotation(desiredDirection);
+            transform.rotation = currentRotation;
+        }
+        else
+        {
+            transform.rotation = currentRotation;
+        }
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+       
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    void AnimatePlayer(Vector3 desiredDirection)
+    {
+        if (!anim)
+            return;
+
+        Vector3 movement = new Vector3(desiredDirection.x, 0f, desiredDirection.z);
+        float forw = Vector3.Dot(movement, transform.forward);
+        anim.SetFloat("Forward", forw);
+    }
+}
---
+++ Assets/InputActions/PlayerProjectileSpawner.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProjectileSpawner : MonoBehaviour
+{
+    public Animator anim;
+
+    [Header("Spawner Settings")]
+    public GameObject projectilePrefab;
+    public Transform spawnPoint;
+    
+    public float spawnRate;
+    private float timer;
+
+    [Header("Particles")]
+    public ParticleSystem spawnParticles;
+
+    [Header("Audio")]
+    public AudioSource spawnAudioSource;
+
+    PlayerInteraction inputActions;
+
+    Vector2 lookPosition;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        inputActions = new PlayerInteraction();
+        inputActions.Player.Fire.performed += ctx => lookPosition = ctx.ReadValue<Vector2>();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(lookPosition.magnitude > 0.1f)
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        if(timer < spawnRate) { return; }
+        timer = 0f;
+        Instantiate(projectilePrefab, spawnPoint.position, spawnPoint.rotation);
+
+        if (spawnParticles)
+        {
+            //spawnParticles.Play();
+        }
+
+        if (spawnAudioSource)
+        {
+            //spawnAudioSource.Play();
+        }
+    }
+}
