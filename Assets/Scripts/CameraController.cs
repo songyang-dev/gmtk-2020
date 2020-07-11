307faadb1a649d08d2307548d7857ff572cba456
Song Yang <songyang1218@gmail.com>
2020-07-11 14:03:38 -0400

    Add camera script and player input controls

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraController : MonoBehaviour
+{
+    public Vector3 OffsetPosition;
+
+    public Vector3 DefaultRotation;
+
+    public void Pan(InputAction.CallbackContext ctx)
+    {
+        
+    }
+}
