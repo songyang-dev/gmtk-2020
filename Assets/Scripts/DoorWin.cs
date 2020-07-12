5b33e843d39acc332fe18a4e29de3716debc5a52
Song Yang <songyang1218@gmail.com>
2020-07-12 13:33:09 -0400

    Add win condition

---
+++ Assets/Scripts/DoorWin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorWin : MonoBehaviour
+{
+    /// <summary>
+    /// Reference to the winning message
+    /// </summary>
+    public GameObject WinMessage;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.gameObject.name.Equals("Player"))
+            Win();
+    }
+
+    private void Win()
+    {
+        WinMessage.SetActive(true);
+    }
+}
